beb22694991f54396ef8d164de0cb974adef48f2
mrtelekinesis <86800445+mrtelekinesis@users.noreply.github.com>
2021-10-26 22:47:00 +0000

    Update buildkernel.sh

--- buildkernel.sh
+++ buildkernel.sh
@@ -15,7 +15,7 @@
 echo "Starting kernel build script"
 
 KBUILD_CPPFLAGS="-U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2 -U_GLIBCXX_ASSERTIONS -D_GLIBCXX_ASSERTIONS"
-KBUILD_CFLAGS="-m64 -w -g0 -O2 -march=native -mtune=native -fomit-frame-pointer -fstack-protector-strong -pipe -fuse-linker-plugin -flto=2 -ffat-lto-objects"
+KBUILD_CFLAGS="-m64 -w -g0 -O2 -march=native -mtune=native -fomit-frame-pointer -fstack-protector-strong -fstack-clash-protection -pipe -fuse-linker-plugin -flto=2 -ffat-lto-objects"
 KBUILD_CXXFLAGS="${KBUILD_CFLAGS} -fvisibility-inlines-hidden"
 KBUILD_LDFLAGS="--as-needed -melf_x86_64 -O2 --sort-common --enable-new-dtags --hash-style=gnu -z combreloc -z now -z relro -z separate-code -z noexecstack -z global -z interpose -z common-page-size=4096 --no-omagic --force-group-allocation --compress-debug-sections=zlib-gnu --build-id=none -flto=2"
 KBUILD_AFLAGS="-Wa,-Os -Wa,-acdn -Wa,-mtune=generic64 -Wa,--strip-local-absolute -Wa,-mrelax-relocations=yes -Wa,-mamd64 -Wa,--64"
